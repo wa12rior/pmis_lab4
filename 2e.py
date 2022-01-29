4bdcb21f0a5629cb2e86fd113f00a0f4e2bb5096
Kamil <wa12rior@gmail.com>
2022-01-29 23:27:21 +0100

    Update 2e.py

--- 2e.py
+++ 2e.py
@@ -3,9 +3,7 @@ import numpy as np
 import cmath as cm
 
 # Definiujemy funkcje
-def xt(t):
-  #d=1
-  d=0.1
+def xt(t, d):
   w2=5/8
   c1 = 1
   c2 = 1
@@ -13,14 +11,20 @@ def xt(t):
 
 domain=np.linspace(1, 100, 100)
 
-y = []
+y1 = []
+y2 = []
+
+d1=0.1
+d2=0.2
 
 for i in domain:
-  y.append(xt(i).astype('float128'))
+  y1.append(xt(i, d1).astype('float128'))
+  y2.append(xt(i, d2).astype('float128'))
 
 fig, ax = plt.subplots()
 
-ax.plot(domain, y)
+ax.plot(domain, y1)
+ax.plot(domain, y2)
 
-plt.legend(['oscylacja'])
+plt.legend(['d=0.1', 'd=0.2'])
 plt.show()
